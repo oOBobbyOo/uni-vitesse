a686f904315ed54220f2683d156ab608defa8183
oOBobbyOo <944831575@qq.com>
2024-02-19 15:44:50 +0800

    chore: change vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import UnoCSS from 'unocss/vite'
 import svgLoader from 'vite-svg-loader'
 // @see https://github.com/vbenjs/vite-plugin-svg-icons
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+// @see https://github.com/antfu/vite-plugin-restart
+import ViteRestart from 'vite-plugin-restart'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,5 +22,30 @@ export default defineConfig({
       // 指定symbolId格式
       symbolId: 'icon-[dir]-[name]',
     }),
+    ViteRestart({
+      // 通过这个插件，在修改vite.config.ts文件则不需要重新运行也生效配置
+      restart: ['vite.config.ts'],
+    }),
   ],
+  server: {
+    host: '0.0.0.0',
+    hmr: true,
+    port: 5173, // 默认端口: 5173
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 })
