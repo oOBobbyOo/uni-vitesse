d45d44bb2027644d68e49fabedd51272200eae9f
oOBobbyOo <944831575@qq.com>
2024-02-19 16:11:01 +0800

    chore: vite.config.ts visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import Uni from '@dcloudio/vite-plugin-uni'
 // @see https://unocss.dev/integrations/vite
 import UnoCSS from 'unocss/vite'
@@ -7,45 +7,71 @@ import UnoCSS from 'unocss/vite'
 import svgLoader from 'vite-svg-loader'
 // @see https://github.com/vbenjs/vite-plugin-svg-icons
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+// @see https://github.com/btd/rollup-plugin-visualizer
+import { visualizer } from 'rollup-plugin-visualizer'
 // @see https://github.com/antfu/vite-plugin-restart
 import ViteRestart from 'vite-plugin-restart'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Uni(),
-    UnoCSS(),
-    svgLoader(),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [resolve(process.cwd(), 'src/assets/svg')],
-      // 指定symbolId格式
-      symbolId: 'icon-[dir]-[name]',
-    }),
-    ViteRestart({
-      // 通过这个插件，在修改vite.config.ts文件则不需要重新运行也生效配置
-      restart: ['vite.config.ts'],
-    }),
-  ],
-  server: {
-    host: '0.0.0.0',
-    hmr: true,
-    port: 5173, // 默认端口: 5173
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5173',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ command, mode }) => {
+  console.log('>>: command', command)
+  // mode: 区分生产环境还是开发环境
+  console.log('>>: mode', mode)
+
+  // process.cwd(): 获取当前文件的目录根地址
+  // loadEnv(): 返回当前环境env文件中额外定义的变量
+  const env = loadEnv(mode, resolve(process.cwd(), 'env'))
+  console.log(env)
+  console.log(process.env.UNI_PLATFORM) // 得到 mp-weixin, h5 等
+
+  return {
+    plugins: [
+      Uni(),
+      UnoCSS(),
+      svgLoader(),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [resolve(process.cwd(), 'src/assets/svg')],
+        // 指定symbolId格式
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      // https://github.com/btd/rollup-plugin-visualizer
+      mode === 'production' &&
+        // 打包分析
+        visualizer({
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+          filename: resolve(process.cwd(), 'node_modules/.cache/.visualizer/stats.html'),
+        }),
+      ViteRestart({
+        // 通过这个插件，在修改vite.config.ts文件则不需要重新运行也生效配置
+        restart: ['vite.config.ts'],
+      }),
+    ],
+    alias: {
+      '@': resolve(process.cwd(), 'src'),
+    },
+    server: {
+      host: '0.0.0.0',
+      hmr: true,
+      port: 5173, // 默认端口: 5173
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5173',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
       },
     },
-  },
+  }
 })
